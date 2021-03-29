643001ef2d0917e76d91f2c5a5e1531ee163549b
KushYadav <kushyadavnitd@gmail.com>
2021-03-29 05:51:06 +0530

    release-demo added

---
+++ release-demo/src/test/java/com/example/releasedemo/ReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
