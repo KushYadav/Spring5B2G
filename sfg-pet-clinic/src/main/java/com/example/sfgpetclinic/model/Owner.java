0172fa5d71f50149645cc6db71330b404bb18409
KushYadav <kushyadavnitd@gmail.com>
2021-03-30 20:08:38 +0530

    POJOs added

---
+++ sfg-pet-clinic/src/main/java/com/example/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.example.sfgpetclinic.model;
+
+/**
+ * Create by Kush on 30-03-2021
+ */
+public class Owner extends Person{
+}
---
+++ sfg-pet-clinic/src/main/java/com/example/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.example.sfgpetclinic.model;
+
+/**
+ * Create by Kush on 30-03-2021
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
