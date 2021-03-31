11d818496e221dbbfec4971ea427251677cc8c1c
KushYadav <kushyadavnitd@gmail.com>
2021-04-01 00:28:10 +0530

    Added vets list

--- sfg-pet-clinic/pet-clinic-web/src/main/java/org/example/sfgpetclinic/controller/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/org/example/sfgpetclinic/controller/VetController.java
@@ -1,6 +1,8 @@
 package org.example.sfgpetclinic.controller;
 
+import org.example.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
