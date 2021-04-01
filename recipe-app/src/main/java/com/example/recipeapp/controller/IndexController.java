417e1af21d173b6d3027c55397c573f87fd550f1
KushYadav <kushyadavnitd@gmail.com>
2021-04-01 16:42:57 +0530

    Added index page

---
+++ recipe-app/src/main/java/com/example/recipeapp/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Create by Kush on 01-04-2021
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","index.html"})
+    public String getIndexPage(Model model){
+        return "index";
+    }
+}
