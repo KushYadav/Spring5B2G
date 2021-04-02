08ee5d1e232973b0abc1184e8e247ad18aa36990
KushYadav <kushyadavnitd@gmail.com>
2021-04-02 22:49:20 +0530

    added visit

--- sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package org.example.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Create by Kush on 30-03-2021
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
