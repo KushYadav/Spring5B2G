e300c0685fd46163505ce0ee1c39cd8070d6de9d
KushYadav <kushyadavnitd@gmail.com>
2021-04-02 22:56:01 +0530

    POJOs updated

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package org.example.sfgpetclinic.model;
+
+/**
+ * Create by Kush on 02-04-2021
+ */
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package org.example.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Create by Kush on 30-03-2021
  */
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
